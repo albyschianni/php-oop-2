ea4a880c25cf65f2d081dbafc7ac1d39c8acd4b2
albyschianni <alberto.schianni@gmail.com>
2023-01-25 17:46:28 +0100

    update

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+include "product.php";
+include "category.php";
+include "typology.php";
+
+$productArr = [new Product("10$","carne","baubau"),new Product("12$","carne2","baubau2"),new Product("13$","carne3","baubau3")];
+
+foreach ($productArr as $newproduct){
+    
+};
+
